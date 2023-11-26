7791c8bab92e4cb0411fec5589198a4393a26d25
Danny <51312422+dchenz@users.noreply.github.com>
2023-11-26 10:07:13 +0000

    Add chakra ui to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,11 @@
-import { useState } from "react";
-import logo from "./assets/images/logo-universal.png";
-import "./App.css";
-import { Greet } from "../wailsjs/go/main/App";
+import { ChakraProvider } from "@chakra-ui/react";
+import LandingPage from "./pages/landing";
 
 function App() {
-  const [resultText, setResultText] = useState(
-    "Please enter your name below 👇"
-  );
-  const [name, setName] = useState("");
-  const updateResultText = (result: string) => setResultText(result);
-
-  function greet() {
-    Greet(name).then(updateResultText);
-  }
-
   return (
-    <div id="App">
-      <img src={logo} id="logo" alt="logo" />
-      <div id="result" className="result">
-        {resultText}
-      </div>
-      <div id="input" className="input-box">
-        <input
-          id="name"
-          className="input"
-          onChange={(e) => setName(e.target.value)}
-          autoComplete="off"
-          name="input"
-          type="text"
-        />
-        <button className="btn" onClick={greet}>
-          Greet
-        </button>
-      </div>
-    </div>
+    <ChakraProvider>
+      <LandingPage />
+    </ChakraProvider>
   );
 }
 
---
+++ frontend/src/pages/landing.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <Box p={3}>
+      <Heading>WSL Desktop</Heading>
+      <Text>Hello world!</Text>
+    </Box>
+  );
+};
+
+export default LandingPage;
