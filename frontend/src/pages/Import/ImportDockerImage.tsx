1befefa7fefb1068f1422458cecbf1fff79b547a
Danny <51312422+dchenz@users.noreply.github.com>
2023-12-25 06:08:47 +0000

    Go to home page when distro is imported

--- frontend/src/pages/Import/ImportDockerImage.tsx
+++ frontend/src/pages/Import/ImportDockerImage.tsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { CreateDistroFromDockerImage } from "../../../wailsjs/go/app/App";
 import { app } from "../../../wailsjs/go/models";
 import Layout from "../../components/Layout";
 import { useErrorToast } from "../../components/toast";
+import { PAGES } from "../../const";
 import ImportDockerImageForm from "./ImportDockerImageForm";
 
 const ImportDockerImage = () => {
   const reportError = useErrorToast();
+  const navigate = useNavigate();
 
   const onSubmit = async (values: app.CreateDistroFromImageRequest) => {
     try {
       await CreateDistroFromDockerImage(values);
+      navigate(PAGES.home);
     } catch (e) {
       reportError(e);
     }
--- frontend/src/pages/Import/ImportTarball.tsx
+++ frontend/src/pages/Import/ImportTarball.tsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { CreateDistroFromTarFile } from "../../../wailsjs/go/app/App";
 import { app } from "../../../wailsjs/go/models";
 import Layout from "../../components/Layout";
 import { useErrorToast } from "../../components/toast";
+import { PAGES } from "../../const";
 import ImportTarballForm from "./ImportTarballForm";
 
 const ImportTarball = () => {
   const reportError = useErrorToast();
+  const navigate = useNavigate();
 
   const onSubmit = async (values: app.CreateDistroFromTarFileRequest) => {
     try {
       await CreateDistroFromTarFile(values);
+      navigate(PAGES.home);
     } catch (e) {
       reportError(e);
     }
